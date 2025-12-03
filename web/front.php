cba9734dbd825c437f317f7ef6141842028222d4
El Hadji NDIAYE <elhadji.ndiaye@sesame-it.fr>
2025-12-03 15:39:51 +0100

    create-framework: Templating

--- web/front.php
+++ web/front.php
@@ -5,37 +5,27 @@ require_once '../vendor/autoload.php';
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
-//use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use Symfony\Component\Routing\Matcher\CompiledUrlMatcher;
-use Symfony\Component\Routing\Matcher\Dumper\CompiledUrlMatcherDumper;
+
+function render_template(Request $request): Response
+{
+    extract($request->attributes->all(), EXTR_SKIP);
+    ob_start();
+    include sprintf(__DIR__ . '/../src/pages/%s.php', $_route);
+
+    return new Response(ob_get_clean());
+}
 
 $request = Request::createFromGlobals();
-$routes = include __DIR__.'/../src/app.php';
+$routes = include __DIR__ . '/../src/app.php';
 
 
 $context = new Routing\RequestContext();
 $context->fromRequest($request);
-
-$compiledRoutes = (new CompiledUrlMatcherDumper($routes))->getCompiledRoutes();
-
-$matcher = new CompiledUrlMatcher($compiledRoutes, $context);
-
-// $generator = new Routing\Generator\UrlGenerator($routes, $context);
-
-// echo $generator->generate('hello', ['name' => 'Fabien']);
-
-// echo $generator->generate(
-//     'hello',
-//     ['name' => 'Fabien'],
-//     UrlGeneratorInterface::ABSOLUTE_URL
-// );
+$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 
 try {
-    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
-    ob_start();
-    include sprintf(__DIR__ . '/../src/pages/%s.php', $_route);
-
-    $response = new Response(ob_get_clean());
+    $request->attributes->add($matcher->match($request->getPathInfo()));
+    $response = call_user_func($request->attributes->get('_controller'), $request);
 } catch (Routing\Exception\ResourceNotFoundException $e) {
     $response = new Response('Not Found', 404);
 } catch (Exception $e) {
