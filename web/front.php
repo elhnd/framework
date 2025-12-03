b53dd0520b6af89a939d34b1219b122d8155180d
El Hadji NDIAYE <elhadji.ndiaye@sesame-it.fr>
2025-12-03 11:10:59 +0100

    create-framework:[200~

---
+++ web/front.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once '../vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$request = Request::createFromGlobals();
+$response = new Response();
+
+$map = [
+    '/hello' => '../src/pages/hello.php',
+    '/bye' => '../src/pages/bye.php'
+];
+
+$path = $request->getPathInfo();
+
+if(isset($map[$path])) {
+    ob_start();
+    require $map[$path];
+    $response->setContent(ob_get_clean());
+} else {
+    $response->setStatusCode(404);
+    $response->setContent('Not Found');
+}
+
+$response->send();
